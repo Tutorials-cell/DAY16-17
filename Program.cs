cfe1dddc97a3ed72e28f97bc909831e1c7b6067e
Bahulya <sarvalabahulya456@gmail.com>
2021-11-24 23:28:28 +0530

    PrimeNumber

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PrimeNumberDemo
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, i, ctr, strno, eeno;
+            strno = 0;
+            eeno = 10;
+            Console.WriteLine(" The prime numbers between {0} and {1} are: \n", strno, eeno);
+            for ( num = strno; num <= eeno; num++)
+            {
+                ctr = 0;
+                for (i = 2; i <= num / 2; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        ctr++;
+                        break;
+                    }
+                }
+                if ( ctr == 0 && num !=1)
+                   
+                        {
+                            Console.Write("{0}", num);
+                        }
+                        Console.WriteLine("\n");
+                }
+                   
+            }
+
+        
+        
+           
+    }
+}
